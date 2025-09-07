4b0b4499d163ccc756bd46b1fac765495bf02360
chrisbryant01 <cbryant#aip.com>
2025-09-06 21:54:06 -0400

    add support for DatabaseError in db.js

--- database.js
+++ database.js
@@ -1,17 +1,32 @@
+import DatabaseError from './DatabaseError.js';
+
 const users = []; // In-memory user storage
 
 const addUser = ({ username, password }) => {
-  users.push({ username, password });
+  if ((username, password)) {
+    users.push({ username, password });
+  } else {
+    throw new DatabaseError('Username and password are required');
+  }
 };
 
 console.log(users); // Logging for testing - should remove in production
 
 const getUser = (user) => {
-  return users.find((u) => u.username === user); // Find user by username
+  if (users.find((u) => u.username === user)) {
+    // Find user by username
+    return users.find((u) => u.username === user);
+  } else {
+    throw new DatabaseError('User not found');
+  }
 };
 
 const getAllUsers = () => {
-  return users;
+  if (users) {
+    return users;
+  } else {
+    throw new DatabaseError('No users found');
+  }
 };
 
 const setPassword = (user, password) => {
@@ -19,7 +34,7 @@ const setPassword = (user, password) => {
   if (foundUser) {
     foundUser.password = password;
   } else {
-    throw new Error('User not found');
+    throw new DatabaseError('User not found');
   }
 };
 
@@ -28,7 +43,7 @@ const deleteUser = (user) => {
   if (index !== -1) {
     users.splice(index, 1); // Remove user from array
   } else {
-    throw new Error('User not found');
+    throw new DatabaseError('User not found');
   }
 };
 
